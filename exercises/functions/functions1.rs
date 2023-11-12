5cea52ad624ce1722bff03584daa0c759d60dc6c
NicoElbers <Ryojipog@gmail.com>
2023-11-12 16:42:59 +0100

    save

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,14 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+use std::io::{self, Write};
+
+fn main() {
+    call_me();
+}
+
+fn call_me() {
+    println!("hello world");
+}
